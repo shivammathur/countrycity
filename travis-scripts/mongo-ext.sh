d6cc60008092c38246a343823bc0aad181ea063c
Shivam Mathur <shivam_jpr@hotmail.com>
2019-09-01 04:30:29 +0530

    newer php versions to travis

--- travis-scripts/mongo-ext.sh
+++ travis-scripts/mongo-ext.sh
@@ -1,4 +1,4 @@
-wget https://pecl.php.net/get/mongodb-1.1.6.tgz
-tar -xzf mongodb-1.1.6
-sh -c "cd mongodb-1.1.6 && phpize && ./configure && sudo make install"
+wget https://pecl.php.net/get/mongodb-1.5.5.tgz
+tar -xzf mongodb-1.5.5
+sh -c "cd mongodb-1.5.5 && phpize && ./configure && sudo make install"
 echo "extension=mongodb.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
