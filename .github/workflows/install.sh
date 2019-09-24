a08469f9d41275d40e0fe2320ef01b54d8e4e2d2
Shivam Mathur <shivam_jpr@hotmail.com>
2019-09-24 21:53:14 +0530

    Remove install.sh and improve README

--- .github/workflows/install.sh
+++
@@ -1,7 +0,0 @@
-sudo add-apt-repository ppa:ondrej/php -y
-sudo apt update -y
-sudo apt install -y php$1 curl
-sudo update-alternatives --set php /usr/bin/php$1
-php -v
-sudo curl -s https://getcomposer.org/installer | php
-sudo mv composer.phar /usr/local/bin/composer
